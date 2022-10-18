5fe27bafd433750984eaee0f88ec95cac0e69a13
9961241736 <vishnump36@gmail.com>
2022-10-18 07:38:53 -0700

    adds all file

--- customer/urls.py
+++ customer/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name='customer'
 urlpatterns=[
-    path('home',views.customer_home,name='home'),
+    path('',views.customer_home,name='home'),
     path('cart',views.customer_cart ,name='cart')
 ]
